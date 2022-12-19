11ee809c26caf17dde2b403fb5557eb9e56b8b46
Dmytro-Yaichenia <dmytro@clouddefense.ai>
2022-12-19 21:12:24 +0200

    feat: add todo list

--- src/app/modules/todo/components/todo-item/todo-item.component.ts
+++ src/app/modules/todo/components/todo-item/todo-item.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from "@angular/core";
+import { TodoInterface } from "../../types/todo.interface";
 
 @Component({
-  selector: 'app-todo-item',
-  templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.scss']
+  selector: "app-todo-item",
+  templateUrl: "./todo-item.component.html",
+  styleUrls: ["./todo-item.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodoItemComponent implements OnInit {
+  @Input() todo: TodoInterface;
+  @Input() index: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onToggleDone(id: string): void {}
+
+  editTodo(id: string): void {}
+
+  deleteTodo(id: string): void {}
 }
--- src/app/modules/todo/components/todo-list/todo-list.component.ts
+++ src/app/modules/todo/components/todo-list/todo-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { TodoService } from "../../services/todo.service";
+import { TodoInterface } from "../../types/todo.interface";
 
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  selector: "app-todo-list",
+  templateUrl: "./todo-list.component.html",
+  styleUrls: ["./todo-list.component.scss"],
 })
 export class TodoListComponent implements OnInit {
+  todoList$: Observable<TodoInterface[]>;
 
-  constructor() { }
+  todoById = (index: number, item: TodoInterface) => item.id;
+
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
+    this.fetchData();
   }
 
+  fetchData(): void {
+    this.todoList$ = this.todoService.getTodos();
+  }
 }
