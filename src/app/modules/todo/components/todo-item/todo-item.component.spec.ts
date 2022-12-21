af5a5ad6e0712413312b3ba6a94e382b901b17c6
Dmytro-Yaichenia <dmytro@clouddefense.ai>
2022-12-21 10:32:09 +0200

    test: todo item component

--- src/app/modules/todo/components/todo-item/todo-item.component.spec.ts
+++ src/app/modules/todo/components/todo-item/todo-item.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { TodoInterface } from '../../types/todo.interface';
 
 import { TodoItemComponent } from './todo-item.component';
+
+const fakeTodo: TodoInterface = {
+  label: "label",
+  description: "description",
+  category: "category",
+  done: false,
+  id: "0",
+};
 
 describe('TodoItemComponent', () => {
   let component: TodoItemComponent;
@@ -16,10 +26,24 @@ describe('TodoItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoItemComponent);
     component = fixture.componentInstance;
+    component.ngOnInit();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit delete when delete is clicked', () => {
+    const deleteSpy = spyOn(component.deleteTodoEvent, 'emit')
+    component.deleteTodo('11')
+    expect(deleteSpy).toHaveBeenCalled()
+  })
+
+  it('should emit toggle when delete is clicked', () => {
+    const deleteSpy = spyOn(component.toggleDoneEvent, 'emit')
+    component.onToggleDone()
+    expect(deleteSpy).toHaveBeenCalled()
+  })
+
 });
